5ff1fd246db972737e93bebbf41a0a921758154f
Haitam Elgharras <elgharras.haitam@gmail.com>
2023-09-28 22:54:24 +0100

    Generics and Primitive types

--- Advanced topics/src/generics/GenericsDemo.java
+++ Advanced topics/src/generics/GenericsDemo.java
@@ -9,11 +9,16 @@ public class GenericsDemo {
         list.add("a");
 
         // what happens at runtime ???
-//        var number = list.get(0);
-//        System.out.println(number);
-//        System.out.println(number.getClass());
-//        var string = list.get(1);
-//        System.out.println(string);
-//        System.out.println(string.getClass());
+        var number = list.get(0);
+        System.out.println(number);
+        System.out.println(number.getClass());
+        var string = list.get(1);
+        System.out.println(string);
+        System.out.println(string.getClass());
+
+        // boxing and unboxing
+        var IntList = new GenericList<Integer>();
+        IntList.add(1); // boxing -> Integer.valueOf(1)
+        int number2 = IntList.get(0); // unboxing -> number2 = IntList.get(0).intValue();
     }
 }
