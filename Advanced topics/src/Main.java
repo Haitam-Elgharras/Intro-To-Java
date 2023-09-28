4917bef9e522dab75da3a944176971eb0330d187
Haitam Elgharras <elgharras.haitam@gmail.com>
2023-09-28 20:29:20 +0100

    Generic classes by poor implimetation

--- Advanced topics/src/Main.java
+++ Advanced topics/src/Main.java
@@ -1,30 +1,39 @@
 import exceptions.Account;
 import exceptions.AccountException;
 import exceptions.ExceptionsDemo;
+import generics.GenericsDemo;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            ExceptionsDemo.show();
-        } catch (Throwable e) {
-            System.out.println("An unexpected error occurred.");
-        }
 
-        Account account = new Account();
-        try {
-            account.deposit(-1);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        // Exceptions
+//        try {
+//            ExceptionsDemo.show();
+//        } catch (Throwable e) {
+//            System.out.println("An unexpected error occurred.");
+//        }
+//
+//        Account account = new Account();
+//        try {
+//            account.deposit(-1);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        try {
+//            account.withdraw(10);
+//        } catch (AccountException e) {
+//            // insufficient funds in your account.
+//            System.out.println(e.getCause().getMessage());
+//        }
 
-        try {
-            account.withdraw(10);
-        } catch (AccountException e) {
-            // insufficient funds in your account.
-            System.out.println(e.getCause().getMessage());
-        }
 
+
+        // Generics
+
+// Generics
+        GenericsDemo.show();
     }
 }
---
+++ Advanced topics/src/generics/GenericsDemo.java
@@ -0,0 +1,19 @@
+package generics;
+
+public class GenericsDemo {
+
+    public static void show() {
+        var list = new List();
+        // we can pass any reference it will be casted to Object with Integer.valueOf(1)...
+        list.add(1);
+        list.add("a");
+
+        // what happens at runtime ???
+        var number = list.get(0);
+        System.out.println(number);
+        System.out.println(number.getClass());
+        var string = list.get(1);
+        System.out.println(string);
+        System.out.println(string.getClass());
+    }
+}
