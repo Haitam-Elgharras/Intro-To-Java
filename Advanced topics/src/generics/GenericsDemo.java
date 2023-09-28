ebecad4eaf159f6bd183c004f48b2f67f26d0381
Haitam Elgharras <elgharras.haitam@gmail.com>
2023-09-28 23:25:23 +0100

    Generics under the hood

--- Advanced topics/src/generics/GenericsDemo.java
+++ Advanced topics/src/generics/GenericsDemo.java
@@ -20,5 +20,12 @@ public class GenericsDemo {
         var IntList = new GenericList<Integer>();
         IntList.add(1); // boxing -> Integer.valueOf(1)
         int number2 = IntList.get(0); // unboxing -> number2 = IntList.get(0).intValue();
+
+        // Generic Constraints: when we've added the constraint <T extends Comparable> we can pass just the types that
+        // implements the Comparable interface
+        var ComparableList1 = new GenericList<Integer>();
+        var ComparableList2 = new GenericList<String>();
+        // var ComparableList3 = new GenericList<Account>(); // Account doesn't implement Comparable interface
+
     }
 }
