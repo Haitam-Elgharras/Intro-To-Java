d6c657fcdb1ff33388d6f3a7215cf699af60e67c
Haitam Elgharras <elgharras.haitam@gmail.com>
2023-11-27 22:25:31 +0100

    Future interface

--- Advanced topics/ExecutiveFramework/src/Main.java
+++ Advanced topics/ExecutiveFramework/src/Main.java
@@ -1,19 +1,58 @@
+import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Main {
     public static void main(String[] args) {
 
-        ExecutorService executor = Executors.newFixedThreadPool(2);
 
+        int [] tab = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21};
+        int nbThreads = 4;
+        int taille = tab.length;
+        int tailleBloc = taille / nbThreads;
+        int reste = taille % nbThreads;
+        int debut = 0;
+        int fin = 0;
+
+        ExecutorService executor= Executors.newFixedThreadPool(nbThreads);
+
+        var futures = new ArrayList<Future<Integer>>();
         try {
-            for (int i = 0; i < 10; i++)
-                executor.submit(() -> {
-                    System.out.println(Thread.currentThread().getName());
+            for (int i = 0; i < nbThreads; i++) {
+                debut = fin;
+                fin = debut + tailleBloc;
+                if (reste > 0) {
+                    fin++;
+                    reste--;
+                }
+                int finalDebut = debut;
+                int finalFin = fin;
+                var future= executor.submit(() -> {
+                    int somme = 0;
+                    for (int j = finalDebut; j < finalFin; j++)
+                        somme += tab[j];
+
+                    return somme;
                 });
+                futures.add(future);
+            }
         } finally {
+            System.out.println("shutdown");
             executor.shutdown();
         }
+
+        // sum
+        int somme = 0;
+        for (var future : futures) {
+            try {
+                // get() is a blocking method
+                somme += future.get();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.printf("Somme = %d\n", somme);// 231
 
     }
 }
